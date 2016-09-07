779dce2e571edf415074cf1db9b377d7112c9568
Andre Przywara <andre.przywara@arm.com>
2016-09-07 01:51:41 +0100

    ARM: vITS: handle DISCARD command
    The DISCARD command drops the connection between a DeviceID/EventID
    and an LPI/collection pair.
    We mark the respective structure entries as not allocated and make
    sure that any queued IRQs are removed.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- xen/arch/arm/vgic-v3-its.c
+++ xen/arch/arm/vgic-v3-its.c
@@ -549,6 +549,34 @@ out_unlock:
     return ret;
 }
 
+static int its_handle_discard(struct virt_its *its, uint64_t *cmdptr)
+{
+    uint32_t devid = its_cmd_get_deviceid(cmdptr);
+    uint32_t eventid = its_cmd_get_id(cmdptr);
+    struct pending_irq *pirq;
+    struct vcpu *vcpu;
+    uint32_t vlpi;
+
+    if ( !read_itte(its, devid, eventid, &vcpu, &vlpi) )
+        return -1;
+
+    pirq = its->d->arch.vgic.handler->lpi_to_pending(its->d, vlpi);
+    if ( pirq )
+    {
+        clear_bit(GIC_IRQ_GUEST_QUEUED, &pirq->status);
+        gic_remove_from_queues(vcpu, vlpi);
+    }
+
+    if ( !write_itte(its, devid, eventid, UNMAPPED_COLLECTION, INVALID_LPI,
+                     NULL) )
+        return -1;
+
+    gicv3_assign_guest_event(its->d, its->doorbell_address,
+                             devid, eventid, NULL, 0);
+
+    return 0;
+}
+
 #define ITS_CMD_BUFFER_SIZE(baser)      ((((baser) & 0xff) + 1) << 12)
 
 /*
@@ -579,6 +607,9 @@ static int vgic_its_handle_cmds(struct domain *d, struct virt_its *its)
         {
         case GITS_CMD_CLEAR:
             ret = its_handle_clear(its, command);
+            break;
+        case GITS_CMD_DISCARD:
+            ret = its_handle_discard(its, command);
             break;
         case GITS_CMD_INT:
             ret = its_handle_int(its, command);
